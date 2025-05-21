b8834ddf0bdc4396ce7f26b7472b368d524dea2a
Lucas William <lucaswilliamml@gmail.com>
2025-05-21 14:37:23 -0300

    password in login section

--- frontend/notes-app/src/pages/Login/Login.tsx
+++ frontend/notes-app/src/pages/Login/Login.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import PasswordInput from "../../components/Input/PasswordInput";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border border-gray-300 rounded bg-white px-7 py-10">
+          <form onSubmit={() => {}}>
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input type="text" placeholder="Email" className="input-box" />
+            <PasswordInput />
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
 
-export default Login
+            <p className="text-sm text-center mt-4">
+              Not registred yet?{" "}
+              <Link to="/signup" className="font-medium text-primary underline">
+                Create an Account
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- frontend/notes-app/src/pages/Signup/Signup.tsx
+++ frontend/notes-app/src/pages/Signup/Signup.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Signup = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
     </>
   );
 };
